7a5f3aa3de2b186e9bb6c4409963606c67451330
butch cosio <butchcosio@yahoo.com>
2018-01-17 19:26:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+  object.prop2 = 2
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj.prop1
+  return newObj
+}
+
+
